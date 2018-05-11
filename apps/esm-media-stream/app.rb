e99cc822b6047f6425218c041bbf4340c9d3ab5d
Supasak <ksupasak@gmail.com>
2018-05-12 00:43:32 +0700

    update endo pacs

--- apps/esm-media-stream/app.rb
+++ apps/esm-media-stream/app.rb
@@ -1,11 +1,63 @@
 
 register_app 'endopacs', 'esm-media-stream'
+require_relative 'services/indexing'
 
 module EsmMediaStream
   
+  include EsmMediaStreamIndex
+  
+  class Session
+    include MongoMapper::Document
+    key :datetime, Time
+    key :solution, String
+    key :name, String
+    key :key, String
+    key :ref, String
+    key :op, String
+    
+  end
+
+  
+  class Video
+    include MongoMapper::Document
+    
+    key :datetime, Time
+    key :name, String
+    key :session_id, ObjectId
+    key :path, String
+    key :raw_size, Integer
+  end
+  
+  
+  
+  class ScanPath
+    include MongoMapper::Document
+    key :name, String
+    key :content_path, String
+    key :path, String
+    
+    
+  end
+  
+  # a = {:solution=>t[2],:key=>key,:op=>op,:id=>id,:name=>name,:track=>track_name, :path=>i, :raw_size=> File.size(i)}
+  
+  class SessionController < GXTDocument
+
+  end
+  
+  class ScanPathController < GXTDocument
+
+  end
+  
+  class VideoController < GXTDocument
+
+  end
   
   class HomeController < GXT
-
+      
+      
+      
+      
   end
   
 end
--- apps/esm-media-stream/services/indexing.rb
+++ apps/esm-media-stream/services/indexing.rb
@@ -1,14 +1,21 @@
-paths = []
+module EsmMediaStreamIndex
+  
+def media_indexing paths
+  
+# paths = []
 
 # paths << "/Volumes/Disk1/room10"
-paths << "/Volumes/Disk1/room11"
+# paths << "/Volumes/Disk1/room11"
 
-for path in paths
+map = {}
 
+
+for pathall in paths
+path = pathall[0]
+content_path = pathall[1]
 list = Dir["#{path}/**/*.mov"]
 puts path
 
-map = {}
 for i in list
   
   t = i[path.size+1..-1].split("/")
@@ -45,7 +52,56 @@ for i in list
   
 end
 
-# puts map.inspect
 
 end
 
+#============
+
+
+map = {}
+
+
+for pathall in paths
+path = pathall[0]
+content_path = pathall[1]
+
+list = Dir["#{path}/**/*.mp4"]
+puts path
+
+for i in list
+  
+  t = i[path.size+1..-1].split("/")
+  folder = i.split("/")[0..-2].join("/")
+  
+  
+  key = t[4].split("-").join("/")
+  op  = t[5]
+  id = t[6]
+  name = t[-1]
+  
+  map[id] = [] unless map[id]
+
+  map_size = map[id].size
+  
+  track_name = "vdo-#{map_size}"
+  
+  pathx = i[content_path.size+1..-1]
+  
+  a = {:solution=>t[2],:key=>key,:op=>op,:id=>id,:name=>name,:track=>track_name, :file_path=>i, :path=>pathx, :raw_size=> File.size(i)}
+ 
+ 
+  map[id] << a
+  
+  
+end
+
+
+end
+
+
+
+return map
+
+end
+
+end
