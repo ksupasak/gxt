e2c2e4f79f51e58f0d1b99bdd25da871f9a4aa90
Supasak <ksupasak@gmail.com>
2021-05-25 00:14:59 +0700

    update

--- apps/esm-miot-monitor/smart_health.rb
+++ apps/esm-miot-monitor/smart_health.rb
@@ -27,7 +27,7 @@ module EsmMiotMonitor
     
       def acl
     
-        return {:patient_upload=>'*',:dashboard=>'*'}
+        return {:patient_upload=>'*',:dashboard=>'*',:spotcheck=>'*'}
     
       end
       
