dc53ca5353d077767307213cbc7340adff96a387
Supasak <ksupasak@gmail.com>
2021-06-10 01:00:59 +0700

    update

--- apps/esm-miot-monitor/app.rb
+++ apps/esm-miot-monitor/app.rb
@@ -252,7 +252,7 @@ class HomeController < GXT
            
            ws.onmessage do |msg_data|
              
-             puts msg_data
+             # puts msg_data
              
              # begin 
               
