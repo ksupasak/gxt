c267262589c8d8f103c970e9d1a9ac6ced5a2059
chanmit.kpm@gmail.com <chanmit.kpm@gmail.com>
2023-02-08 17:20:15 +0700

    STEMI

--- apps/esm-miot-monitor/views/ems_emr/documents/lib.rb
+++ apps/esm-miot-monitor/views/ems_emr/documents/lib.rb
@@ -9,6 +9,10 @@ def check_string this, name, options={}
   return inline(this,'documents/_render_field', :this=>this, :name=>name, :options=>options, :type=>'check_string')
 end
 
+def select_string this, name, options={}
+  return inline(this,'documents/_render_field', :this=>this, :name=>name, :options=>options, :type=>'select_string')
+end
+
 def text_string this, name, options={}
   return inline(this,'documents/_render_field', :this=>this, :name=>name, :options=>options, :type=>'text_string')
 end
