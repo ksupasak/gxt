38542ea33c5a32aafada0c9a3c6da67d0415a61c
Supasak Kulawonganunchai <ksupasak@gmail.com>
2023-04-12 23:22:03 +0700

    Update app.rb

--- apps/esm-miot-monitor/app.rb
+++ apps/esm-miot-monitor/app.rb
@@ -317,6 +317,18 @@ class HomeController < GXT
 
   end
 
+  @@redisx_connection = nil
+
+  def redisx
+    
+    unless @@redisx_connection
+      c = "redis://#{":"+REDIS_PASS+"@" if REDIS_PASS}#{REDIS_HOST}:#{REDIS_PORT}/#{REDIS_DB}"
+      puts "REDIS CONFIG SERVICE 1 : #{c}"
+      @@redisx_connection = EM::Hiredis.connect c
+    end
+  
+    return @@redisx_connection
+  end
 
   def websocket request
 
@@ -324,9 +336,7 @@ class HomeController < GXT
 
        EM.run do
 
-                      c = "redis://#{":"+REDIS_PASS+"@" if REDIS_PASS}#{REDIS_HOST}:#{REDIS_PORT}/#{REDIS_DB}"
-                      puts "REDIS CONFIG SERVICE 1 : #{c}"
-                      redisx = EM::Hiredis.connect c
+                     
 
         request.websocket do |ws|
 
