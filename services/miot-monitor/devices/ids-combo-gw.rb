185269b8b503f9b9ac320125ce9c35f15807e59f
Supasak Kulawonganunchai <ksupasak@gmail.com>
2023-07-24 11:29:45 +0700

    update

--- services/miot-monitor/devices/ids-combo-gw.rb
+++ services/miot-monitor/devices/ids-combo-gw.rb
@@ -319,7 +319,7 @@ EOM
       puts "weight = #{current_weight}, height = #{current_height} tweigth = #{trig_weight} #{device_id}"
      
    
-if current_weight  and current_weight.to_f > 0
+if true || current_weight  and current_weight.to_f > 0
    
       lines = []
 
