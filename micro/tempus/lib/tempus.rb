1fc8b5eeaf20df219cc631be7eff4b37d3fde392
Supasak Kulawonganunchai <ksupasak@gmail.com>
2023-09-27 23:13:53 +0700

    tempus

---
+++ micro/tempus/lib/tempus.rb
@@ -0,0 +1,280 @@
+require 'net/http'
+require "openssl"
+require 'digest/sha2'
+require 'base64'
+require 'json'
+
+
+module Tempus
+  
+  def self.decode64 encode
+      return encode.unpack('m')[0]
+  end
+  
+  class Gateway
+    
+    attr :url
+    attr :username
+    attr :password
+    attr :key
+    attr :iv
+    attr :decode_cipher
+    
+    
+    
+    attr :token
+    attr :refresh_token
+    
+    attr :expired_in
+    
+    attr :organization_id
+    attr :organization_name
+    
+    
+    def initialize settings
+      
+      puts 'init'
+      
+      if settings[:key]
+        
+      set_decription(settings[:key], settings[:iv])   
+      @url = settings[:url]
+      @username = decode(settings[:username])
+      @password = decode(settings[:password])
+      @token = nil
+      
+      else
+      
+      @url = settings[:url]
+      @username = settings[:username]
+      @password = settings[:password]
+      @token = nil
+      
+      end
+      
+      
+      puts "Init #{@url} #{@username} #{@password}"
+      
+      
+      token = get_token
+      
+    end
+    
+    def decode encrypt64
+       encrypt = Tempus::decode64(encrypt64)       
+       plain = @decode_cipher.update(encrypt)
+       plain << @decode_cipher.final
+       @decode_cipher.reset
+       return plain.split("\u0000").join    
+    end
+    
+    def set_decription key, iv
+      
+      @key = Tempus::decode64(key)
+      @iv = Tempus::decode64(iv)
+      alg = "AES-256-CBC"
+      @decode_cipher = OpenSSL::Cipher::Cipher.new(alg)
+      @decode_cipher.decrypt
+      @decode_cipher.key = @key
+      @decode_cipher.iv = @iv
+     
+    end
+    
+    def set_token result
+      
+      # puts "set_token #{result.inspect }"
+      @token = result['Access_token']
+      @refresh_token = result['Refresh_token']
+      @expired_in = Time.at(Time.now.to_i + result['Expires_in']) if result['Expires_in']
+      set_decription(result['K'], result['V'])
+      
+    end
+    
+    def get_token
+      
+      unless @token
+        
+        uri = URI("#{@url}/Account/APILogin")
+        
+        req = Net::HTTP::Post.new(uri, {'Accept'=>'application/json','Content-Type' =>'application/json'})
+        p = {}
+        p['Username'] = @username
+        p['Password'] = @password
+        p['DEM'] = "AES"
+        p['Key'] = ""
+       
+        
+        req.body = p.to_json
+   
+        res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl=>true) {|http|
+          http.request(req)
+        }
+
+        # puts res.body
+
+        result = JSON.parse(res.body)['Data']
+        
+        set_token result
+        
+      end
+      
+      return @token
+      
+    end
+    
+    def get_live_incidents
+      
+      
+      data = get_data "/Api/Clinical/LiveIncidents?organizationId=#{@organization_id}", nil
+      
+      return data
+      
+      
+    end
+    
+    def get_incidents params
+      
+      fromDate=Time.now.to_date.at_beginning_of_day
+      toDate=Time.now
+      
+      device_id = nil 
+      device_id = params[:device_id]
+      
+      fromDate=params[:from_date] if params[:from_date]
+      toDate=params[:to_date] if params[:to_date]
+      
+      fromDate = fromDate.strftime("%Y-%m-%dT%H:%M:%S")
+      toDate = toDate.strftime("%Y-%m-%dT%H:%M:%S")
+      
+      uri = "/Api/Clinical/Incidents?organizationId=#{@organization_id}&fromDate=#{fromDate}&toDate=#{toDate}"
+      uri << "&tempusSerialNumber=#{device_id}" if device_id
+      
+   
+      data = get_data uri, nil
+      
+      return data
+      
+      
+    end
+    
+    
+    def get_organization 
+      
+      data = get_data "/Api/Organizations", nil
+     
+      first_org = data[0]
+      @organization_id =   first_org['OrganizationId']
+      @organization_name = first_org['Name']
+      
+      return @organization_id
+      
+    end
+    
+    def get_trended_vitals incident_id, datetime=nil
+
+      unless datetime
+        return get_data "/Api/Clinical/Incidents/#{incident_id}/TrendedVitals", nil
+      else
+        return get_data "/Api/Clinical/Incidents/#{incident_id}/LiveTrendedVitals?toDate=#{datetime}", nil
+      end
+        # return data
+      
+    end
+
+
+    def get_events incident_id, datetime=nil
+
+      unless datetime
+        return get_data "/Api/Clinical/Incidents/#{incident_id}/Events", nil
+      else
+        return get_data "/Api/Clinical/Incidents/#{incident_id}/LiveEvents?toDate=#{datetime}", nil
+      end
+        # return data
+      
+    end
+    
+    def download_twelve_leads incident_id, twelve_leads_id, filename
+# TWELVE_LEADS
+        data = get_data "/Api/Clinical/Incidents/#{incident_id}/Events/TwelveLead/#{twelve_leads_id}?Format=JPEG", nil
+        puts data.size
+        if data['TwelveLeadData']
+        
+          out = File.open(filename, 'w')
+          out.write data['TwelveLeadData'].unpack('m')[0]
+          out.flush
+          out.close
+          
+        
+        end
+        
+        
+      
+    end
+    
+    def download_pdf incident_id, filename
+# TWELVE_LEADS
+        data = get_data "/Api/Clinical/Incidents/#{incident_id}/PDF", nil
+        # puts data.to_json
+        if data['Data']
+        
+          out = File.open(filename, 'w')
+          out.write data['Data'].unpack('m')[0]
+          out.flush
+          out.close
+          
+        
+        end
+        
+        
+      
+    end
+
+    
+    
+    def get_data uri, raw
+      
+      token = get_token
+      
+      puts uri
+ 
+      
+      uri = URI("#{@url}#{uri}")
+      
+      
+
+      req = Net::HTTP::Get.new(uri, {'Accept'=>'application/json','Content-Type'=>'application/json','Authorization'=>"bearer #{token}"})
+
+      res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl=>true) {|http|
+        http.request(req)
+      }
+      
+     # puts res.body #if raw
+      # puts res.body.size
+      
+      data = JSON.parse(res.body)
+      if data['IsSuccessful'] 
+        
+        if raw==nil
+          data = JSON.parse(decode(data['Data']))
+        else
+          data = decode(data['Data'])
+        end
+        
+        return data
+        
+      else
+        return nil
+        
+      end
+        
+    end
+    
+    
+    def run
+      
+    end
+    
+  end
+  
+  
+end
---
+++ micro/tempus/start_gateway.rb
@@ -0,0 +1,306 @@
+require_relative 'lib/tempus'
+require_relative 'lib/ws'
+require "active_support"
+require "eventmachine"
+require "active_support/core_ext/date/calculations"
+require 'sinatra'
+require 'sinatra/base'
+# gw = Tempus::Gateway.new :key=> 'nRHprq9d0Y9UefyxJ3buzZp0Do5RpFtacxzDyRK77VQ=', :iv=>'Nki18CKUFhH5CrI3TAaWdg==', :username=>'A5yPKnq8yLJKwtxOnyfhACcsWVpUYUj63VbsDCTdy+D+GGa4rnJATz1bvnPCU+/8', :password=>'gcbfEBUB3nQc+nCsjMknLiqmk+lbbF6l0Leuv+ZY+1I=', :url=>'https://corsium.com.au/WebAPI/chula'
+
+require 'fileutils'
+
+
+
+
+def run(opts)
+
+  # Start he reactor
+  EM.run do
+
+    # define some defaults for our app
+    server  = opts[:server] || 'thin'
+    host    = opts[:host]   || '0.0.0.0'
+    port    = opts[:port]   || '4567'
+    web_app = opts[:app]
+
+    dispatch = Rack::Builder.app do
+      map '/' do
+        run web_app
+      end
+    end
+
+    # NOTE that we have to use an EM-compatible web-server. There
+    # might be more, but these are some that are currently available.
+    unless ['thin', 'hatetepe', 'goliath'].include? server
+      raise "Need an EM webserver, but #{server} isn't"
+    end
+
+    # Start the web server. Note that you are free to run other tasks
+    # within your EM instance.
+    Rack::Server.start({
+      app:    dispatch,
+      server: server,
+      Host:   host,
+      Port:   port,
+      signals: false,
+    })
+    
+    
+    lives = web_app.settings.live
+    
+    gw = web_app.settings.gw
+    ws = web_app.settings.ws
+       
+    EventMachine.add_periodic_timer(3) {
+ 
+      # puts "run #{Time.now}"
+      
+    
+    
+      
+      
+      online_lives = gw.get_live_incidents
+      
+      current = {}
+      # puts online_lives.size
+      for i in online_lives
+      
+         id = i['IncidentId']
+         current[id] = id
+         lives[id] = {} unless lives[id]
+         lives[id] = i
+        
+      end
+      
+      lives.each_pair do |k,v|
+        
+        unless current[k]
+          
+          lives.delete k 
+          
+        end
+        
+      end
+        
+        
+      puts "Current #{lives.keys.size }"
+       
+      
+    }
+    
+    
+    
+    
+    EventMachine.add_periodic_timer(10) {
+    
+      if lives.keys.size>0 
+        
+        
+        lives.each_pair do |k,v|
+          puts k
+          datetime = Time.now.strftime("%Y-%m-%dT%H:%M:%S")
+          
+          # vitals = gw.get_trended_vitals k, datetime
+          vitals = gw.get_trended_vitals k , datetime
+          
+          
+          
+          GXTWS.send ws, v, vitals
+          
+          
+          
+          
+          events = gw.get_events k , datetime
+          
+          
+          puts events['EventsInfo'].size.to_s + " #{Time.now.to_f-ts.to_f}"
+  
+          twelve_leads_id = nil
+  
+          for i in events['EventsInfo']
+            puts "#{i['EventId']} #{i['EventType']}"
+            if i['EventType'] == 'TWELVE_LEADS'
+              twelve_leads_id = i['EventId']  
+              # puts i['Vitals'].inspect
+            end
+          end
+  
+          if twelve_leads_id and File.exists?(File.join("sent","#{twelve_leads_id}.jpg"))==false
+            
+            gw.download_twelve_leads id, twelve_leads_id, File.join("data","#{twelve_leads_id}.jpg")
+            GXTWS.send_image ws, v, File.join("data","#{twelve_leads_id}.jpg")
+            FileUtils.mv(File.join("data","#{twelve_leads_id}.jpg"), File.join("sent","#{twelve_leads_id}.jpg"))
+
+          end
+          
+          # puts vitals.to_json
+          
+          # {"DataCaptureTimeUtc"=>"2023-09-14T08:10:00", "Vitals"=>[{"Name"=>"HR", "Value"=>"100", "Measurement"=>"bpm", "Code"=>"", "Source"=>"ECG", "Alert"=>false}, {"Name"=>"SpO2", "Value"=>"96", "Measurement"=>"%", "Code"=>"", "Source"=>"PULSE", "Alert"=>false}, {"Name"=>"Resp", "Value"=>"39", "Measurement"=>"rpm", "Code"=>"", "Source"=>"CAPNO", "Alert"=>false}, {"Name"=>"ETCO2", "Value"=>"43", "Measurement"=>"mmHg", "Code"=>"", "Source"=>"CAPNO", "Alert"=>false}, {"Name"=>"T1", "Value"=>"36.4", "Measurement"=>"C", "Code"=>"", "Source"=>"T1", "Alert"=>false}, {"Name"=>"T2", "Value"=>"35.8", "Measurement"=>"C", "Code"=>"", "Source"=>"T2", "Alert"=>false}, {"Name"=>"PI", "Value"=>"3.7", "Measurement"=>"%", "Code"=>"", "Source"=>"PULSE", "Alert"=>false}, {"Name"=>"PVI", "Value"=>"33", "Measurement"=>"%", "Code"=>"", "Source"=>"PULSE", "Alert"=>false}, {"Name"=>"SpOC", "Value"=>"19", "Measurement"=>"ml/dl", "Code"=>"", "Source"=>"PULSE", "Alert"=>false}, {"Name"=>"SpHb", "Value"=>"12.2", "Measurement"=>"g/dl", "Code"=>"", "Source"=>"PULSE", "Alert"=>false}, {"Name"=>"SpCO", "Value"=>"6", "Measurement"=>"%", "Code"=>"", "Source"=>"PULSE", "Alert"=>false}, {"Name"=>"SpMet", "Value"=>"1.7", "Measurement"=>"%", "Code"=>"", "Source"=>"PULSE", "Alert"=>false}]}
+            #
+          #
+          # for i in vitals['VitalDetailsInfo']
+          #
+          #    detail = i['Vitals'].collect{|j| "#{j["Name"]} #{j["Value"]}"}.join(", ")
+          #
+          #    puts "#{i['DataCaptureTimeUtc']}\t#{detail}}"
+          #
+          # end
+          #
+         
+            
+        end
+        
+        
+        
+      end
+    
+    
+    }
+    
+    
+  end
+end
+
+
+
+
+# Our simple hello-world app
+class HelloApp < Sinatra::Base
+  
+  
+
+  
+  
+  
+  
+  # threaded - False: Will take requests on the reactor thread
+  #            True:  Will queue request for background thread
+  configure do
+    set :threaded, false
+    
+    gw = Tempus::Gateway.new :key=> 'nRHprq9d0Y9UefyxJ3buzZp0Do5RpFtacxzDyRK77VQ=', :iv=>'Nki18CKUFhH5CrI3TAaWdg==', :username=>'A5yPKnq8yLJKwtxOnyfhACcsWVpUYUj63VbsDCTdy+D+GGa4rnJATz1bvnPCU+/8', :password=>'gcbfEBUB3nQc+nCsjMknLiqmk+lbbF6l0Leuv+ZY+1I=', :url=>'https://corsium.com.au/WebAPI/chula'
+    # gw = Tempus::Gateway.new :username=>'686d4517-bd7f-4b8d-9ece-7a6ce799ad6f', :password=>'3P(RTe$t1n9', :url=>'https://corsium.info/WebAPI/sdkencr'
+    
+    set :gw, gw
+    
+    org_id = gw.get_organization
+    
+    set :org_id, org_id
+    
+    set :live, {}
+    
+     opts = {}
+    
+    ws_host = opts[:ws_host] || 'pcm-life.com'
+    ws_solution = opts[:ws_solution] || 'miot'
+
+    ws = GXTWS::connect ws_solution, ws_host
+    
+    GXTWS::bind_event ws
+    
+    set :ws, ws
+    
+  end
+  
+ 
+
+  # Request runs on the reactor thread (with threaded set to false)
+  get '/' do
+    
+     @org = settings.org_id
+     
+     erb :home
+     
+  end
+
+  # Request runs on the reactor thread (with threaded set to false)
+  # and returns immediately. The deferred task does not delay the
+  # response from the web-service.
+  get '/delayed-hello' do
+    EM.defer do
+      sleep 5
+      puts 'ok'
+    end
+    'I\'m doing work in the background, but I am still free to take requests'
+  end
+end
+
+run app: HelloApp.new
+
+#
+# EventMachine.run {
+#
+#
+#   EventMachine.add_periodic_timer(3) {
+#
+#
+#
+#
+#   }
+#
+#
+# }
+
+
+#
+#
+#
+# # incidents = gw.get_incidents datetime
+# incidents = gw.get_incidents :device_id=>'111002', :from_date => Time.now.to_date.at_beginning_of_month
+#
+#
+# puts incidents.inspect
+#
+#
+# # https://corsium.info/WebAPI/sdk/Api/Clinical/Incidents/FA68370A-BF24-4BD1-99BE- DF946C8ECDDE/TrendedVitals
+#
+# incidents.size
+#
+# if incidents.size > 0
+#
+#   id = incidents[0]['IncidentId']
+#
+#
+#
+#   while(true)
+#
+#   ts = Time.now
+#   datetime = Time.now.strftime("%Y-%m-%dT%H:%M:%S")
+#   # vitals = gw.get_trended_vitals id, datetime
+#   # puts vitals['VitalDetailsInfo'].size.to_s + " #{Time.now.to_f-ts.to_f}"
+#
+#   events = gw.get_events id #, datetime
+#   puts events['EventsInfo'].size.to_s + " #{Time.now.to_f-ts.to_f}"
+#
+#   twelve_leads_id = nil
+#
+#   for i in events['EventsInfo']
+#     puts "#{i['EventId']} #{i['EventType']}"
+#     if i['EventType'] == 'TWELVE_LEADS'
+#       twelve_leads_id = i['EventId']
+#       # puts i['Vitals'].inspect
+#     end
+#   end
+#
+#   if twelve_leads_id
+#
+#     gw.download_twelve_leads id, twelve_leads_id, "#{twelve_leads_id}.jpg"
+#
+#
+#   end
+#
+#   # gw.download_pdf id, 'report.pdf'
+#
+#
+#   # vitals = gw.get_trended_vitals id
+#
+#   # puts vitals.inspect
+#
+#
+#   sleep(0.01)
+#
+#   end
+#
+#
+# end
+
